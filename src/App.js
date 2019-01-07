bbd258d33928bdc444c2ab0a593f60120f675a58
vmirella <vmirella_17@hotmail.com>
2019-01-07 03:48:07 -0500

    Realizando maqueta del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container form-main">
+        <div className="row">
+          <div className="col-md-6">
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Nombre</Label>
+              <Input type="text" placeholder="Ingrese aquí su nombre." />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleEmail">Apellido</Label>
+              <Input type="text" placeholder="Ingrese aquí su apellido." />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleEmail">Correo</Label>
+              <Input type="email" placeholder="Ingrese aquí su correo." />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelect">Cargo</Label>
+              <Input type="select">
+                <option>Analista funcional</option>
+                <option>Técnico</option>
+                <option>Secretaria</option>
+                <option>Seo</option>
+                <option>Administrador</option>
+              </Input>
+            </FormGroup> 
+            <FormGroup>
+              <Label for="exampleDate">Fecha de ingreso</Label>
+              <Input
+                type="date"
+                placeholder="Ingrese la fecha de ingreso."
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleFile">Foto</Label>
+              <Input type="file"/>
+              <FormText color="muted">
+                Seleccione una foto para adjuntarla a su carnet.
+              </FormText>
+            </FormGroup>          
+          </Form>
+          </div>
+          <div className="col-md-6">
+            <div className="preview">
+              <FormGroup>
+                <Label>Vista Previa</Label>
+                <p></p>
+              </FormGroup>
+              <Button>Descargar</Button>
+            </div>
+          </div>
+        </div>        
       </div>
     );
   }
