dd8b2da82bc930fe6e9c5eecab668467c1f4c766
vmirella <vmirella_17@hotmail.com>
2019-01-07 12:54:53 -0500

    Updates

---
+++ precache-manifest.0143fe406c2385d0aaccf380c56ccce4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e3144e8d619892709b0",
+    "url": "/reto-divemotors/static/js/runtime~main.8e3144e8.js"
+  },
+  {
+    "revision": "3bd44cdd3c2b41e43318",
+    "url": "/reto-divemotors/static/js/main.3bd44cdd.chunk.js"
+  },
+  {
+    "revision": "285f18977e031bdaeca2",
+    "url": "/reto-divemotors/static/js/1.285f1897.chunk.js"
+  },
+  {
+    "revision": "3bd44cdd3c2b41e43318",
+    "url": "/reto-divemotors/static/css/main.65fd8e20.chunk.css"
+  },
+  {
+    "revision": "9dc4358756075a0f273f83382aaa2058",
+    "url": "/reto-divemotors/index.html"
+  }
+];
