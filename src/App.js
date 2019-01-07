f5c2a305c80a92353fa0cce0af86c21767a766bc
vmirella <vmirella_17@hotmail.com>
2019-01-07 10:24:49 -0500

    Funcionalidades de generar foto para carnet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import domtoimage from 'dom-to-image';
+import ReactDOM from 'react-dom';
+import FileSaver from 'file-saver';
 import './App.css';
 
 class App extends Component {
   
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      lastname: '',
+      email: '',
+      position: '',
+      dateAdmission: '',
+      dateAdmissionFormat: '',
+      image: null,
+      errorMessage: ''
+    }
+  }
+
+  handleChangeName = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  handleChangeLastname = (e) => {
+    this.setState({lastname: e.target.value});
+  }
+
+  handleChangeEmail = (e) => {
+    this.setState({email: e.target.value});
+  }
+
+  handleChangePosition = (e) => {
+    this.setState({position: e.target.value});
+  }
+
+  handleChangeDateAdmission = (e) => {
+    this.setState({dateAdmission: e.target.value});
+    let date = e.target.value;
+    let stringDate = date.split('-');
+    this.setState({dateAdmissionFormat: stringDate[2] + '/' + stringDate[1] + '/' + stringDate[0]});
+  }
+
+  handleChangeImage = (e) => {
+    this.setState({image: URL.createObjectURL(e.target.files[0])});
+    this.setState({errorMessage: ''});
+  }
+
+  generatePhoto = () => {
+    if(this.state.image === null) {
+      this.setState({errorMessage: 'No se ha seleccionado una imagen.'});
+      return;
+    }
+
+    this.setState({errorMessage: ''});
+    let photo = ReactDOM.findDOMNode(this.refs['photo']);
+
+    domtoimage.toBlob(photo)
+    .then(function (blob) {
+      FileSaver.saveAs(blob, 'carnet.png');
+    });
+  }
+
   render() {
     return (
       <div className="container form-main">
@@ -11,37 +71,37 @@ class App extends Component {
           <div className="col-md-6">
           <Form>
             <FormGroup>
-              <Label for="exampleEmail">Nombre</Label>
-              <Input type="text" placeholder="Ingrese aquí su nombre." />
+              <Label>Nombre</Label>
+              <Input onChange={e => this.handleChangeName(e)} value={this.state.name} type="text" placeholder="Ingrese aquí su nombre." />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleEmail">Apellido</Label>
-              <Input type="text" placeholder="Ingrese aquí su apellido." />
+              <Label>Apellido</Label>
+              <Input onChange={e => this.handleChangeLastname(e)} value={this.state.lastname} type="text" placeholder="Ingrese aquí su apellido." />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleEmail">Correo</Label>
-              <Input type="email" placeholder="Ingrese aquí su correo." />
+              <Label>Correo</Label>
+              <Input onChange={e => this.handleChangeEmail(e)} value={this.state.email}
+              type="email" placeholder="Ingrese aquí su correo." />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleSelect">Cargo</Label>
-              <Input type="select">
-                <option>Analista funcional</option>
-                <option>Técnico</option>
-                <option>Secretaria</option>
-                <option>Seo</option>
-                <option>Administrador</option>
+              <Label>Cargo</Label>
+              <Input onChange={e => this.handleChangePosition(e)} value={this.state.position} type="select">
+                <option value="">Seleccione el cargo...</option>
+                <option value="Analista funcional">Analista funcional</option>
+                <option value="Técnico">Técnico</option>
+                <option value="Secretaria">Secretaria</option>
+                <option value="Seo">Seo</option>
+                <option value="Administrador">Administrador</option>
               </Input>
             </FormGroup> 
             <FormGroup>
-              <Label for="exampleDate">Fecha de ingreso</Label>
-              <Input
-                type="date"
-                placeholder="Ingrese la fecha de ingreso."
+              <Label>Fecha de ingreso</Label>
+              <Input onChange={e => this.handleChangeDateAdmission(e)} value={this.state.dateAdmission} type="date" placeholder="Ingrese la fecha de ingreso."
               />
             </FormGroup>
             <FormGroup>
-              <Label for="exampleFile">Foto</Label>
-              <Input type="file"/>
+              <Label>Foto</Label>
+              <Input onChange={e => this.handleChangeImage(e)} type="file"/>
               <FormText color="muted">
                 Seleccione una foto para adjuntarla a su carnet.
               </FormText>
@@ -52,22 +112,23 @@ class App extends Component {
             <div className="preview">
               <FormGroup>
                 <Label>Vista Previa</Label>
-                <div className="view-preview">
+                <div ref={'photo'} className="container-fluid view-preview">
                   <div className="row">
-                    <div className="col-md-4">
-                      <img src=""/>
+                    <div className="col-sm-4 preview-image">
+                      <img alt="" src={this.state.image}/>
                     </div>
-                    <div className="col-md-8">
-                      <p>Nombre: </p>
-                      <p>Apellido: </p>
-                      <p>Correo: </p>
-                      <p>Cargo: </p>
-                      <p>Ingreso: </p>
+                    <div className="col-sm-8 preview-data">
+                      <p>Nombre: {this.state.name}</p>
+                      <p>Apellido: {this.state.lastname}</p>
+                      <p>Correo: {this.state.email}</p>
+                      <p>Cargo: {this.state.position}</p>
+                      <p>Ingreso: {this.state.dateAdmissionFormat}</p>
                     </div>
                   </div>
                 </div>
               </FormGroup>
-              <Button>Descargar</Button>
+              <Button onClick={this.generatePhoto}>Descargar</Button>
+              <p>{this.state.errorMessage}</p>
             </div>
           </div>
         </div>        
