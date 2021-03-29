e01543b195c8fcbec3cf50924dd4c63ee5882499
vmirella <vmirella_17@hotmail.com>
2021-03-29 01:45:36 -0500

    Updates

---
+++ precache-manifest.a4947b30a538599594c4f41161b152d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "550bd8cb398ec34aebdc",
+    "url": "/reto-divemotors/static/css/main.edfc43fb.chunk.css"
+  },
+  {
+    "revision": "550bd8cb398ec34aebdc",
+    "url": "/reto-divemotors/static/js/main.550bd8cb.chunk.js"
+  },
+  {
+    "revision": "1edca1d7a1f3c3cc63c4",
+    "url": "/reto-divemotors/static/js/1.1edca1d7.chunk.js"
+  },
+  {
+    "revision": "8e3144e8d619892709b0",
+    "url": "/reto-divemotors/static/js/runtime~main.8e3144e8.js"
+  },
+  {
+    "revision": "6947dd29ca0fc5c69efb96a1291abf2c",
+    "url": "/reto-divemotors/index.html"
+  }
+];
